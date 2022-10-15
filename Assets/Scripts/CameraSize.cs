2bf73a98181ad15313c5021dd81878f29d21d79c
Todoran Ciprian Bogdan <bogdan.todoran@quadom.com>
2022-10-15 20:53:13 +0300

    Camera resize on player scale

--- Assets/Scripts/CameraSize.cs
+++ Assets/Scripts/CameraSize.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CameraSize : MonoBehaviour
@@ -7,11 +9,10 @@ public class CameraSize : MonoBehaviour
 
     // Player
     [SerializeField] private Transform _player;
-    private float _playerInitialScaleX;
-
 
     [SerializeField] private Camera _camera;
-    [SerializeField] private float _minCameraSize = 2.5f;
+    [SerializeField] private float _minCameraSize = 2.0f;
+    [SerializeField] private bool _scalingCamera = false;
 
     #endregion --------------------------------------- Fields ------------------------------------
 
@@ -21,22 +22,54 @@ public class CameraSize : MonoBehaviour
     {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _camera = this.gameObject.GetComponent<Camera>();
-
-        _playerInitialScaleX = _player.transform.localScale.x;
     }
 
     private void Update()
     {
-        ScaleCameraSize();
+        if ((_camera.orthographicSize < (_player.transform.localScale.x + 1.0f - 0.2f)) && !_scalingCamera)
+        { 
+            StartCoroutine(ScaleUPCameraSize());
+            _scalingCamera = true;
+        }
+        if ((_camera.orthographicSize > (_player.transform.localScale.x + 1.0f + 0.2f)) && !_scalingCamera)
+        {
+            StartCoroutine(ScaleDOWNCameraSize());
+            _scalingCamera = true;
+        }
     }
 
     #endregion --------------------------------------- Mono ------------------------------------
 
     #region ------------------------------------------ Mono ------------------------------------
 
-    private void ScaleCameraSize()
+    private IEnumerator ScaleUPCameraSize()
     {
-        _camera.orthographicSize = _minCameraSize + (_player.transform.localScale.x - _playerInitialScaleX);
+        yield return new WaitForSecondsRealtime(0.001f);
+        if (_camera.orthographicSize < (_player.transform.localScale.x + 1.0f))
+        {
+            _camera.orthographicSize += 0.001f;
+            StartCoroutine(ScaleUPCameraSize());
+        }
+        if (_camera.orthographicSize <= (_player.transform.localScale.x + 1.0f + 0.5f) && _camera.orthographicSize >= (_player.transform.localScale.x + 1.0f - 0.5f))
+        {
+            _scalingCamera = false;
+            StopAllCoroutines();
+        }
+    }
+
+    private IEnumerator ScaleDOWNCameraSize()
+    {
+        yield return new WaitForSecondsRealtime(0.001f);
+        if (_camera.orthographicSize > (_player.transform.localScale.x + 1.0f))
+        {
+            _camera.orthographicSize -= 0.001f;
+            StartCoroutine(ScaleDOWNCameraSize());
+        }
+        if (_camera.orthographicSize <= (_player.transform.localScale.x + 1.0f + 0.5f) && _camera.orthographicSize >= (_player.transform.localScale.x + 1.0f - 0.5f))
+        {
+            _scalingCamera = false;
+            StopAllCoroutines();
+        }
     }
 
     #endregion --------------------------------------- Mono ------------------------------------
