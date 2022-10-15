e74c8f34b3beb7d3d5407a80bc63ceabe61a799c
Ionut Tasca <ionut.tasca@quadom.com>
2022-10-15 14:08:38 +0300

    player

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+//Script by Ionut
+//Version: 0.0.1
+//What does this script do?
+
+using UnityEngine;
+
+
+namespace Framework.Custom
+{
+    ///<summary>
+    /// What does this script do?
+    ///</summary>
+
+    public class PlayerStats: MonoBehaviour
+    {
+        public float fatScore = 0f;
+        public float health = 100;
+        public PlayerController playerController;
+
+
+        public CircleCollider2D collider2D;
+
+
+    }
+}
